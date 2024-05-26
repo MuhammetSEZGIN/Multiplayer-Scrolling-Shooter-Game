d820c6271d8ad8d477cf0a85a90b27bc71a30289
Muammer Bayar <80854016+muammerbayar@users.noreply.github.com>
2024-05-26 23:30:06 +0300

    update

---
+++ Multiplayer-Scrolling-Shooter-Game/client-side/src/main/java/org/example/client/ClientRequestType.java
@@ -0,0 +1,5 @@
+package org.example.client;
+
+public enum ClientRequestType {
+    move, shoot, startGame, chat, joinLobby, createLobby, getLobbies,
+}
---
+++ Multiplayer-Scrolling-Shooter-Game/server-side/src/main/java/org/example/server/ServerResponseType.java
@@ -0,0 +1,5 @@
+package org.example.server;
+
+public enum ServerResponseType {
+    lobbiesList,lobbyUpdate, lobbyCreated, chat, gameState, lobbyJoinState
+}
